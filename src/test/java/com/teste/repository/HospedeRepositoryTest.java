5584171cfe9e3bb172aaac8a17c6629986e7b6ff
Matheus <matheus.physicist369@gmail.com>
2024-10-01 11:18:17 -0300

    Repository

---
+++ src/test/java/com/teste/repository/HospedeRepositoryTest.java
@@ -0,0 +1,117 @@
+package com.teste.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.teste.entity.Hospede;
+
+@DataJpaTest
+class HospedeRepositoryTest {
+	@Autowired
+	private HospedeRepository hospedeRepository;
+	
+	@DisplayName("Testando o s Save")
+	@Test
+	void testSalvarRepository() {
+		
+		//Given /Arrange
+		Hospede hospede1 = new Hospede(null, "Julia Maria",
+											"julia@gmail.com",
+											"(00)0000-0000");
+		//When /Act
+		Hospede saveHospede = hospedeRepository.save(hospede1);
+		
+		//Then /Assert
+		assertNotNull(saveHospede);
+		assertTrue(saveHospede.getId() > 0);
+	}
+	@DisplayName("Testando o GET")
+	@Test
+	void testGetAllRepository() {
+		
+		//Given /Arrange
+		Hospede hospede1 = new Hospede(null, "Julia Maria",
+											"julia@gmail.com",
+											"(00)0000-0000");
+		
+		Hospede hospede2 = new Hospede(null, "Julia Alberto",
+				"julio@gmail.com",
+				"(00)0000-0000");
+		
+		hospedeRepository.save(hospede1);
+		hospedeRepository.save(hospede2);
+		
+		List<Hospede> hospedeList = hospedeRepository.findAll();
+		//Then /Assert
+		assertNotNull(hospedeList);
+		assertEquals(2, hospedeList.size());
+	}
+	@DisplayName("Testando o GET By Id")
+	@Test
+	void testGetById() {
+		
+		//Given /Arrange
+		Hospede hospede1 = new Hospede(null, "Julio Fernando",
+											"julio@gmail.com",
+											"(00)0000-0000");
+		
+		hospedeRepository.save(hospede1);
+		
+		Hospede saveHospede = hospedeRepository.findById(hospede1.getId()).get();
+		
+
+		//Then /Assert
+		assertNotNull(saveHospede);
+		assertEquals(hospede1.getId(), saveHospede.getId());
+	}
+	@DisplayName("Testando o UPDATE")
+	@Test
+	void testUpdateHospede() {
+		
+		//Given /Arrange
+		Hospede hospede1 = new Hospede(null, "Julio Fernando",
+											"julio@gmail.com",
+											"(00)0000-0000");
+		
+		hospedeRepository.save(hospede1);
+		
+		Hospede saveHospede = hospedeRepository.findById(hospede1.getId()).get();
+		hospede1.setNome("Josué");
+		hospede1.setEmail("josuegameplays@gmail.com");
+		
+		Hospede updateHospede = hospedeRepository.save(saveHospede);
+
+		//Then /Assert
+		assertNotNull(updateHospede);
+		assertEquals("Josué", updateHospede.getNome());
+		assertEquals("josuegameplays@gmail.com", updateHospede.getEmail());
+	}
+	
+	@DisplayName("Testando o DELETE")
+	@Test
+	void testDeleteHospede() {
+		
+		//Given /Arrange
+		Hospede hospede1 = new Hospede(null, "Julio Fernando",
+											"julio@gmail.com",
+											"(00)0000-0000");
+		
+		hospedeRepository.save(hospede1);
+		
+		hospedeRepository.deleteById(hospede1.getId());
+		
+		Optional<Hospede> hospedeOptional = hospedeRepository.findById(hospede1.getId());
+		
+		assertTrue(hospedeOptional.isEmpty());
+	}
+}
+
